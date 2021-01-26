4b4a7593a9769ea023359c85d5cd3db096aff374
konbert <38583919+konbert@users.noreply.github.com>
2021-01-26 20:56:07 +0100

    Application redesigned to requirements of homework 9

--- source/es6/todoapp/todoapp.js
+++ source/es6/todoapp/todoapp.js
@@ -16,73 +16,235 @@ if ( todoStorage.getObj("users") != null) {
 } else {
 	users = [];
 }
-let lists = [];
-let activeListIndex = -1;
-let newList = false;
 
+// global variables
 // Login status and user
 let logged_in = false;
 let activeUserIndex = -1;
 let activeUser = {};
+// todo list
+let lists = [];
+let activeListIndex = -1;
+let newList = false;
 
-// HTML Elements
-const indexPane = document.getElementById("index");
-const menuPane = document.getElementById("menu");
-const loginPane = document.getElementById("login");
-const registerPane = document.getElementById("register");
-const dashboardPane = document.getElementById("dashboard");
-const dashboardList = document.getElementById("dashboard_list");
-const listPane = document.getElementById("list");
-const checkListTable = document.getElementById("check_list_table");
-const userSettingsPane = document.getElementById("settings");
-const messageLine = document.getElementById("message");
+///////////////////////////////////////////////
+//
+//	function definitions for site construction
+//
+///////////////////////////////////////////////
 
-// EventListeners
-indexPane.addEventListener("click", logReg);
-menuPane.addEventListener("click", menu);
-loginPane.addEventListener("click", login);
-registerPane.addEventListener("click", registerHandler);
-dashboardPane.addEventListener("click", dashboardHandler);
-listPane.addEventListener("click", listHandler);
-userSettingsPane.addEventListener("click", settingsHandler)
+const body = document.getElementById("body");
 
-// functions	
+function createBody() {
+	body.innerHTML = (`
+	<div id="top">
+		<table style="width:100%">
+			<tr>
+				<td><h1>TODO-LIST</h1></td>
+				<td align="right"><div id="menu" hidden>
+					<b id="active_user"></b>
+					<button type="button" class="button-secondary">account settings</button>
+					<button type="button" class="button-negative">logout</button>
+				</div>
+				</td>
+			</tr>
+		</table>
+	</div>
 
-// handler for menu
-function menu(e) {
-	switch (e.target.innerText) {
-		case "logout":
-			logged_in = false;
-			activeUser = {};
-			menuPane.setAttribute("hidden", true);
-			switchPane(indexPane);
-			break;
-		case "account settings":
-			let fields = userSettingsPane.querySelectorAll("#settingsForm input");
-			fields[0].value = activeUser.firstname; 
-			fields[1].value = activeUser.lastname; 
-			fields[2].value = activeUser.email; 
-			fields[3].value = activeUser.pwd;
+	<div id="status">
+	</div>
 
-			switchPane(userSettingsPane);
-			break;
-	}
+	<div id="main">
+	</div>
+
+	<div id="status">
+		<h3 id="message" class="message"></h3>
+	</div>
+
+	`);
+	const menuPane = document.getElementById("menu");
+	menuPane.addEventListener("click", menuHandler);
 }
 
-// handler function to switch to login or register
+function showIndexPane() {
+	const main = document.getElementById("main");
+	main.innerHTML = (`
+		<div id="index">
+		<h3>A very simple TODO-Application.</h3>
+
+		<div><i>Build with HTML, CSS and JavaScript as a single-page-app<br>
+			 using only local storage (no files, no db)</i></div>
+		<br>
+
+		<div>Please log in or sign up yourself.</div>
+
+		<form id="logreg">
+			<button type="button" class="button-primary">Log in</button>
+			<button type="button" class="button-primary">Sign Up</button>
+		</form>
+	</div>
+	`)
+	const indexPane = document.getElementById("index");
+	indexPane.addEventListener("click", logReg);
+}
+
+function showSignUpPane() {
+	const main = document.getElementById("main");
+	main.innerHTML = (`
+	<div id="sign_up">
+		<h3>Sign Up new User</h3>
+		<form id="registerForm">
+			<table>
+				<tr>
+					<td>First name:</td><td><input name="firstname"></td>
+				</tr>
+				<tr>
+					<td>Last name:</td><td><input name="lastname"></td>
+				</tr>
+				<tr>
+					<td>Email:</td><td> <input name="email"></td>
+				</tr>
+				<tr>
+					<td>Password:</td><td> <input type="password" name="password"></td>
+				</tr>
+				<tr>
+					<td align="right"><input type="checkbox" id="agree"></td><td>I agree to the Terms of Use</td>
+				</tr>
+				<tr><td></td>
+					<td>
+						<button type="button" class="button-positive">register</button>
+						<button type="button" class="button-primary">back</button>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	`);
+	const signUpPane = document.getElementById("sign_up");
+	signUpPane.addEventListener("click", registerHandler )
+}
+
+function showLoginPane() {
+	const main = document.getElementById("main");
+	main.innerHTML = (`
+	<div id="login">
+		<h3>Login</h3>
+		<form id="loginForm">
+			<table>
+				<tr>
+					<td>Email:</td><td> <input name="email"></td>
+				</tr>
+				<tr>
+					<td>Password:</td><td> <input type="password" name="password"></td>
+				</tr>
+				<tr><td></td>
+					<td>
+						<button type="button" class="button-positive">login</button>
+						<button type="button" class="button-primary">back</button>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	`)
+	const loginPane = document.getElementById("login");
+	loginPane.addEventListener("click", loginHandler);
+}
+
+function showDashboardPane() {
+	const main = document.getElementById("main");
+	main.innerHTML = (`
+	<div id="dashboard" >
+		<h3>Dashboard</h3>
+		<h3>Your lists:</h3>
+		<ul id="dashboard_list">
+
+		</ul>
+		<button type="button" class="button-primary">Create New to-do List</button><br><br>
+	</div>
+	`);
+	const dashboardPane = document.getElementById("dashboard");
+	dashboardPane.addEventListener("click", dashboardHandler);
+}
+
+function showListPane() {
+	const main = document.getElementById("main");
+	main.innerHTML = (`
+	<div id="list">
+		<h3>To-Do-List</h3>
+		Listname: <input name="list_name" id="list_name" ><br><br>
+		<table>
+			<tbody id="check_list_table">
+				<tr><th>check</th><th>item</th></tr>
+			</tbody>
+		</table>
+		<br>
+		<input name="new_item" id="new_item">
+		<button type="button" class="button-secondary">new item</button>
+		<br>
+		<button type="button" class="button-positive">save</button>
+		<button type="button" class="button-primary">back to dashboard</button>
+	</div>
+	`);
+	const listPane = document.getElementById("list");
+	listPane.addEventListener("click", listHandler);
+}
+
+function showUserSettingsPane() {
+	const main = document.getElementById("main");
+	main.innerHTML = (`
+	<div id="user_settings">
+		<h3>Account settings</h3>
+		<form id="settingsForm">
+			<table>
+				<tr>
+					<td>First name:</td><td><input name="firstname"></td>
+				</tr>
+				<tr>
+					<td>Last name:</td><td><input name="lastname"></td>
+				</tr>
+				<tr>
+					<td>Email:</td><td> <input name="email"></td>
+				</tr>
+				<tr>
+					<td>Password:</td><td> <input type="password" name="password"></td>
+				</tr>
+				<tr><td></td>
+					<td>
+						<button type="button" class="button-positive">save</button>
+						<button type="button" class="button-primary">back to dashboard</button>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	`);
+	const userSettingsPane = document.getElementById("user_settings");
+	userSettingsPane.addEventListener("click", userSettingsHandler);
+}
+
+
+///////////////////////////////////////////////
+//
+//	Event handler functions 
+//
+///////////////////////////////////////////////
+
 function logReg(e) {
 	switch (e.target.innerText) {
 	case "Log in":
-		switchPane(loginPane);
+		showLoginPane();
 		break;
 	case "Sign Up":
-		switchPane(registerPane);
+		showSignUpPane();
 	} 
 }
 
 // handler function for registration form
 function registerHandler(e) {
-	let fields = registerPane.querySelectorAll("#registerForm input");
+	const signUpPane = document.getElementById("sign_up");
+	const messageLine = document.getElementById("message");
+	let fields = signUpPane.querySelectorAll("#registerForm input");
 	switch (e.target.innerText) {
 	case "register":
 		if ( fields[0].value === "" | 
@@ -105,57 +267,24 @@ function registerHandler(e) {
 			todoStorage.setObj("users", users );
 			
 			// update site .. goto login
-			switchPane( loginPane);
+			showLoginPane();
 		}
 		break;
 	case "back":
+		messageLine.innerText = "";
 		for (let i=0; i<4; i++) {
 			fields[i].value = "";
 		}
-		switchPane(indexPane);
-	}
-}
-
-// handler function for registration form
-function settingsHandler(e) {
-	let fields = userSettingsPane.querySelectorAll("#settingsForm input");
-	switch (e.target.innerText) {
-	case "save":
-		if ( fields[0].value === "" | 
-			 fields[1].value === "" |
-			 fields[2].value === "" |
-			 fields[3].value === "" ) {
-			messageLine.innerText = "Error - all fields must be filled!";
-		} else {
-			messageLine.innerText = "";
-			// get data from form
-			let myUser = { firstname: fields[0].value, 
-						   lastname: fields[1].value, 
-				           email: fields[2].value, 
-				           pwd: fields[3].value        };
-			users[activeUserIndex] = myUser;
-			activeUser = myUser;
-			
-			//store data
-			todoStorage.setObj("users", users );
-			
-			// update site .. goto dashboard
-			backToDashboard();
-		}
-		break;
-	case "back to dashboard":
-		fields[0].value = "";
-		fields[1].value = "";
-		fields[2].value = "";
-		fields[3].value = "";
-
-		backToDashboard();
-		break;
+		showIndexPane();
 	}
 }
 
 // handler function for login form
-function login(e) {
+function loginHandler(e) {
+	const loginPane = document.getElementById("login");
+	const messageLine = document.getElementById("message");
+	const menuPane = document.getElementById("menu");
+	
 	if (e.target.innerText === "login") {
 
 		// get data from form
@@ -169,7 +298,8 @@ function login(e) {
 					logged_in = true;
 					messageLine.innerText = "";
 					activeUserIndex = index;
-					activeUser = {firstname: u.firstname, lastname: u.lastname, email: u.email, pwd: u.pwd};
+					//activeUser = {firstname: u.firstname, lastname: u.lastname, email: u.email, pwd: u.pwd};
+					activeUser = u;
 					// delete login form
 					fields[0].value = "";
 					fields[1].value = "";
@@ -180,43 +310,101 @@ function login(e) {
 			if (!logged_in) {
 				messageLine.innerText = "Login failed :: unknown user or wrong password!";
 			} else {
+				messageLine.innerText = "";
 				menuPane.removeAttribute("hidden");
 
 				// update site .. goto dashboard
+				showDashboardPane();
 				populateDashboard();			
 				document.getElementById("active_user").innerText = activeUser.email;
-				switchPane(dashboardPane);
+				
 			}
 		} else {
 			messageLine.innerText = "Error - there are no known users, please register."
-			switchPane(indexPane);
+			showIndexPane();
 		}
+	} else if (e.target.innerText === "back") {
+		messageLine.innerText = "";
+		showIndexPane();
 	}
+}
+
+// Populate Dashboard with todo lists of active user;
+function populateDashboard() {
+	const dashboardList = document.getElementById("dashboard_list");
+	
+	// get data for current user from localStorage
+	if (todoStorage.getObj(activeUser.email)) {
+		lists = todoStorage.getObj(activeUser.email);
+
+		for (let item of lists) {
+			let newLI = document.createElement("li");
+			newLI.innerText = item.name;
+			dashboardList.appendChild(newLI);
+		}
+	}	
+	
 }
 
 // handler for click in the dashboard
 function dashboardHandler(e) {
 	if (e.target.innerText === "Create New to-do List") {
 		// simple case: display emtpy list
-		deleteList();
 		let activeList = document.getElementById("list_name");
 		activeList.value = "";
 		newList = true;
+		showListPane();
 	} else {
 		if (e.target.type = "li") {
 			// clicked on list item
 			newList = false;
+			showListPane();
 			populateList(e.target.innerText);
 		}
+	}	
+}
+
+// Populate list view with selected todo list
+function populateList(listname) {
+	const messageLine = document.getElementById("message");
+	const checkListTable = document.getElementById("check_list_table");
+
+	if (lists === []) {
+		messageLine.innerText = "Error - no list found";
+	} else {
+		//deleteList();
+		let index = 0;
+		messageLine.innerText = "";
+		// find the list in the lists
+		for (let list of lists) {
+			if (list.name === listname) {
+				let activeList = document.getElementById("list_name");
+				activeList.value = listname;
+				activeListIndex = index;
+				for (let item of list.items) {
+					//appendCheckListTable(item.item, item.checked);
+					let newRow = document.createElement("tr");
+					newRow.innerHTML = (`
+						<td><input type="checkbox" ${item.checked ? "checked" : ""}></td>
+						<td>${item.item}</td>
+					`);				
+					checkListTable.appendChild(newRow);
+				}
+			}
+			index ++;
+		}
 	}
-	switchPane(listPane);
 }
 
 // handler for click in the list view
 function listHandler(e) {
+	const messageLine = document.getElementById("message");
+	const checkListTable = document.getElementById("check_list_table");
+
 	switch (e.target.innerText) {
 		case "back to dashboard":
-			backToDashboard();
+			showDashboardPane();
+			populateDashboard();
 			break;
 		case "new item":
 			let newItem = document.getElementById("new_item");
@@ -225,7 +413,14 @@ function listHandler(e) {
 			} else {
 				messageLine.innerText = "";
 			
-				appendCheckListTable(newItem.value, false);
+				//appendCheckListTable(newItem.value, false);
+				let newRow = document.createElement("tr");
+				newRow.innerHTML = (`
+					<td><input type="checkbox" false></td>
+					<td>${newItem.value}</td>
+				`);				
+				checkListTable.appendChild(newRow);
+
 				newItem.value = "";
 				newItem.focus();
 			}
@@ -276,98 +471,71 @@ function listHandler(e) {
 	}
 }
 
-function backToDashboard() {
-	deleteList();
-	populateDashboard();
-	switchPane(dashboardPane);
+// handler for menu
+function menuHandler(e) {
+	const menuPane = document.getElementById("menu");
+	switch (e.target.innerText) {
+		case "logout":
+			logged_in = false;
+			activeUser = {};
+			menuPane.setAttribute("hidden", true);
+			showIndexPane();
+			break;
+		case "account settings":
+			showUserSettingsPane();
+			const userSettingsPane = document.getElementById("user_settings");
+			let fields = userSettingsPane.querySelectorAll("#settingsForm input");
+			fields[0].value = activeUser.firstname; 
+			fields[1].value = activeUser.lastname; 
+			fields[2].value = activeUser.email; 
+			fields[3].value = activeUser.pwd;
+			break;
+	}
 }
 
-// Populate Dashboard with todo lists of active user;
-function populateDashboard() {
-	if (logged_in && activeUser.email != "") {
-		// delete current content
-		let childList = dashboardList.querySelectorAll("li");
-		for (let item of childList) {
-			dashboardList.removeChild(item);
+// handler function for registration form
+function userSettingsHandler(e) {
+	const userSettingsPane = document.getElementById("user_settings");
+	const messageLine = document.getElementById("message");
+
+	let fields = userSettingsPane.querySelectorAll("#settingsForm input");
+	switch (e.target.innerText) {
+	case "save":
+		if ( fields[0].value === "" | 
+			 fields[1].value === "" |
+			 fields[2].value === "" |
+			 fields[3].value === "" ) {
+			messageLine.innerText = "Error - all fields must be filled!";
+		} else {
+			messageLine.innerText = "";
+			// get data from form
+			let myUser = { firstname: fields[0].value, 
+						   lastname: fields[1].value, 
+				           email: fields[2].value, 
+				           pwd: fields[3].value        };
+			users[activeUserIndex] = myUser;
+			activeUser = myUser;
+			
+			//store data
+			todoStorage.setObj("users", users );
+			
+			// update site .. goto dashboard
+			showDashboardPane();
+			populateDashboard();
 		}
-
-		// get data for current user from localStorage
-		if (todoStorage.getObj(activeUser.email)) {
-			lists = todoStorage.getObj(activeUser.email);
-
-			for (let item of lists) {
-				let newLI = document.createElement("li");
-				newLI.innerText = item.name;
-				dashboardList.appendChild(newLI);
-			}
-		}	
+		break;
+	case "back to dashboard":
+		showDashboardPane();
+		break;
 	}
 }
 
-// delete list view
-function deleteList() {
-	let childList = checkListTable.querySelectorAll("tr");
-	let index = 0;
-	for (let item of childList) {
-		if (index != 0) {
-			checkListTable.removeChild(item);
-		}
-		index++;
-	}
-}
+///////////////////////////////////////////////
+//
+//	Run the application
+//
+///////////////////////////////////////////////
+// MAIN: 
 
-// Populate list view with selected todo list
-function populateList(listname) {
-	if (lists === []) {
-		messageLine.innerText = "Error - no list found";
-	} else {
-		deleteList();
-		let index = 0;
-		messageLine.innerText = "";
-		// find the list in the lists
-		for (let list of lists) {
-			if (list.name === listname) {
-				let activeList = document.getElementById("list_name");
-				activeList.value = listname;
-				activeListIndex = index;
-				for (let item of list.items) {
-					appendCheckListTable(item.item, item.checked);
-				}
-			}
-			index ++;
-		}
-	}
-}
-
-// Append check-list-table with new entry
-function appendCheckListTable(item, checked) {
-	let newRow = document.createElement("tr");
-	
-	let colChecked = document.createElement("td");
-	let newCheckBox = document.createElement("input")
-	newCheckBox.type = "checkbox";
-	if (checked === true) {
-		newCheckBox.setAttribute("checked", true);
-	}
-	colChecked.appendChild(newCheckBox);
-	newRow.appendChild(colChecked);
-	
-	let colItem = document.createElement("td");
-	colItem.innerText = item;
-	newRow.appendChild(colItem);
-
-	checkListTable.appendChild(newRow);
-}
-
-// switch application pane
-function switchPane(applicationPane) {
-	indexPane.setAttribute("hidden", true);
-	loginPane.setAttribute("hidden", true);
-	registerPane.setAttribute("hidden", true);
-	dashboardPane.setAttribute("hidden", true);
-	listPane.setAttribute("hidden", true);
-	userSettingsPane.setAttribute("hidden", true);
-	messageLine.innerText = "";
-
-	applicationPane.removeAttribute("hidden");
-}
+createBody();
+showIndexPane();
